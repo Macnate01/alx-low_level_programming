edf0d85d7b4d43ab8b8f732acfdeb8ca664109ec
Macnate01 <macnate87@gmail.com>
2022-10-17 18:37:15 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
 
 /**
- *
  *  main - Entry point
  *  Description: print 0, 1, - 9
- *
  *  Return: Always 0 (Success)
  */
+
 int main(void)
 {
-
 	int digit = 0;
 	while (digit <= 9)
 	{
