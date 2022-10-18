f93b44713b880ce04205fc54cc8ea7db7a563e10
Macnate01 <macnate87@gmail.com>
2022-10-17 18:21:32 -0700

    Task 10

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
 /**
- *  main-progrtam entry point
- *  Return:0 success, non zero fail
- */
+* main - Entry point
+*
+* Description: print 0, 1, - 9
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int m, i, j;
-	for (i = '0'; i <= '9'; i++)
-	{
-		for (j = '0'; j <= '9'; j++)
-	{
-		for (m = '0'; m <= '9'; m++)
-	{
-		if (i < j && j < m)
-	{
-		putchar(i);																putchar(j);																putchar(m);
-		if (i == '7' && j == '8' && m == '9')
-	{
-		break;																}																		else;																																	{
-		putchar(',');																putchar(' ');																																}																																		}
 	
-	}																}		}
-		putchar ('\n');	
-		return (0);
-}
+	int digit = 0;
+	
+	while (digit <= 9)
+	
+	{
+	
+	putchar(digit + 48);
+	
+	if (digit != 9)
+	
+	{
+	
+	putchar(',');
+	
+	putchar(' ');
+	
+	}
+	
+	++digit;
+	
+	}
+	
+	putchar('\n');
+	
+	return (0);
+	
+	}
